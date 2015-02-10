4fded993702eadfee83e47e046aee0deb5c6c7a3
Alex Li <ali@perforce.com>
2015-02-10 00:48:10 -0800

    temporarily disable modernizr, since only within web container, the javascript can be loaded.

--- client/js/canvasapp.js
+++ client/js/canvasapp.js
@@ -3,22 +3,52 @@
  */
 window.onload = canvasApp();
 
-function canvasApp(){
-  var theCanvas = document.getElementById("canvas");
+function canvasApp() {
+//    if(!canvasSupport()){
+//        return;
+//    }
 
-  if(!theCanvas || !theCanvas.getContext){
-    return;
-  }
+    var theCanvas = document.getElementById("canvas");
 
-  drawScreen();
-  function drawScreen(){
+    console.log(theCanvas)
 
-  }
+    if (!theCanvas || !theCanvas.getContext) {
+        console.log("0");
+
+        return;
+    }
+
+    console.log("1");
+
+    drawScreen();
+
+    console.log("3");
+
+    function drawScreen() {
+        console.log("2");
+        var context = theCanvas.getContext("2d");
+
+        // background
+        context.fillStyle = "#ffffaa";
+        context.fillRect(0,0,500,300);
+
+        // text
+        context.fillStyle = "#000000";
+        context.font = "20px _sans";
+        context.textBaseline = "top";
+        context.fillText("Hello Tetris!",195,80);
+
+        //image
+
+        // box
+        context.strokeStyle = "#000000";
+        context.strokeRect(5,5,490,290);
+    }
 
 }
 
-function canvasSupport(){
-  return Modernizr.canvas;
+function canvasSupport() {
+    return Modernizr.canvas;
 }
 
 
