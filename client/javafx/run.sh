8fc46c3f7946b6c204b9459df9c73dc507450d67
Alex Li <cdli99@yahoo.com>
2015-02-11 11:23:27 -0800

    update run.sh to always favorite the classes than the jar.

--- client/javafx/run.sh
+++ client/javafx/run.sh
@@ -1,6 +1,16 @@
-# Either
+#!/bin/bash
+
 # export JAVA_HOME=`/usr/libexec/java_home -v 1.8.0_25`
-java -cp target/tetris-1.0.jar com.yisoft.javafx.tetris.TetrisApp &
-# Or
-# mvn exec:java -Dexec.mainClass="com.yisoft.javafx.TetrisApp"
+
+if [ -f target/classes/com/yisoft/javafx/tetris/TetrisApp.class ]; then
+	java -cp target/classes com.yisoft.javafx.tetris.TetrisApp &
+else
+	if [ ! -f target/tetris-*.jar ]; then
+		mvn package
+	fi
+	java -cp target/tetris-*.jar com.yisoft.javafx.tetris.TetrisApp &
+fi
+
+# alternatively
+# mvn exec:java -Dexec.mainClass="com.yisoft.javafx.tetris.TetrisApp"
 
